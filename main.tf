9aa2b74fda6f298f63aa04b7842812f7c8053b6c
Aberlia-Devops <a.berlia891@gmail.com>
2025-06-18 13:30:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
  secret_key = "TRZ/VRpDt2GAk2EGuq7Ah27tb2NpaDCsO2x5nNmh"
 }
 resource "aws_instance" "ec2_demo" {
- ami           = "ami-00b7ea845217da02c"
+ ami           = "ami-0b09627181c8d5778"
  instance_type = "t2.micro"
  tags = {
    Name = "Jenkins-Terraform-EC2"
