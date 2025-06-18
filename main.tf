2d97a3c705a08be9b3745de63e304e8e942c71c0
Aberlia-Devops <a.berlia891@gmail.com>
2025-06-18 13:16:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
  region     = "us-east-1"
- access_key = jwo1bQ2k9I8HRC20nG63zd8jj479/rf6btUPqdEf
- secret_key = AKIAQFLZDMDB4T2Z5JW5
+ access_key = "AKIAQFLZDMDB4T2Z5JW5"
+ secret_key = "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"
 }
 resource "aws_instance" "ec2_demo" {
  ami           = ami-00b7ea845217da02c
