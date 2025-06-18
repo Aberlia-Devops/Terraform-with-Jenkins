c5fa395c889da21cf82b1cd4335986ef8e18f77b
Aberlia-Devops <a.berlia891@gmail.com>
2025-06-18 12:46:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+ region     = "us-east-1"
+ access_key = jwo1bQ2k9I8HRC20nG63zd8jj479/rf6btUPqdEf
+ secret_key = AKIAQFLZDMDB4T2Z5JW5
+}
+resource "aws_instance" "ec2_demo" {
+ ami           = ami-00b7ea845217da02c
+ instance_type = t2.micro
+ tags = {
+   Name = "Jenkins-Terraform-EC2"
+ }
+}
+
